cf2da1d80f4a90dd07b3408c9e95b4a6b375938f
Jenifer López <jenifer.lopez@adevinta.com>
2025-01-27 23:34:47 +0100

    Add login with Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import auth from 'auth-astro';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind(), auth()],
+  output: 'server',
+  adapter: vercel(),
 });
---
+++ auth.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import Google from '@auth/core/providers/google';
+
+export default defineConfig({
+    providers: [
+        Google({
+            clientId: import.meta.env.GOOGLE_CLIENT_ID,
+            clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET
+        })
+    ]
+})
